c2c72b0029c0905bc2f1d6785884eba2f40a7d12
willen <willen.sesantos@gmail.com>
2023-05-25 12:09:33 -0300

    Department entity class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import model.entities.Department;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
     }
 }
