8e540be4b25ae46f325b305a75e236ce381012f0
willen <willen.sesantos@gmail.com>
2023-05-26 18:36:15 -0300

    Reusing instantiation

--- src/db/DbIntegrityException.java
+++ src/db/DbIntegrityException.java
@@ -1,8 +1,6 @@
 package db;
 
 public class DbIntegrityException extends RuntimeException {
-    private static final long serialVesionUID = 1L;
-
     public DbIntegrityException(String msg) {
         super(msg);
     }
--- src/model/dao/imp/SellerDaoJDBC.java
+++ src/model/dao/imp/SellerDaoJDBC.java
@@ -53,17 +53,8 @@ public class SellerDaoJDBC implements SellerDao {
             rs = st.executeQuery();
 
             if (rs.next()) {
-                Department dep = new Department();
-                dep.setId(rs.getInt("DepartmentId"));
-                dep.setName(rs.getString("DepName"));
-
-                Seller obj = new Seller();
-                obj.setId(rs.getInt("Id"));
-                obj.setName(rs.getString("Name"));
-                obj.setEmail(rs.getString("Email"));
-                obj.setSalary(rs.getDouble("BaseSalary"));
-                obj.setBirthDate(rs.getDate("BirthDate"));
-                obj.setDepartment(dep);
+                Department dep = instantiateDepartment(rs);
+                Seller obj = instanteteSeller(rs, dep);
 
                 return obj;
             }
@@ -76,6 +67,24 @@ public class SellerDaoJDBC implements SellerDao {
             // Não precisamos fechar a conexão porque outros métodos do objeto podem fazer
             // uso da conexão
         }
+    }
+
+    private Seller instanteteSeller(ResultSet rs, Department dep) throws SQLException {
+        Seller obj = new Seller();
+        obj.setId(rs.getInt("Id"));
+        obj.setName(rs.getString("Name"));
+        obj.setEmail(rs.getString("Email"));
+        obj.setSalary(rs.getDouble("BaseSalary"));
+        obj.setBirthDate(rs.getDate("BirthDate"));
+        obj.setDepartment(dep);
+        return obj;
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString("DepName"));
+        return dep;
     }
 
     @Override
