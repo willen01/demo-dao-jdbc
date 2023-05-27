51ee19e7d4e5976e8dec98d1bee417c736c2596f
willen <willen.sesantos@gmail.com>
2023-05-27 17:00:45 -0300

    Update implementation

--- src/App.java
+++ src/App.java
@@ -35,5 +35,11 @@ public class App {
 
         sellerDao.insert(newSeller);
         System.out.println("Insert! new id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update =====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
