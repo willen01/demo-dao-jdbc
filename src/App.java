7678475e72f3bb1a2794bd1d5a606979f4cb740b
willen <willen.sesantos@gmail.com>
2023-05-27 15:54:09 -0300

    Insert implementation

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -22,11 +23,17 @@ public class App {
             System.out.println(obj);
         }
 
-        System.out.println("\n=== TEST 3: seller seller findAll =====");
+        System.out.println("\n=== TEST 3: seller findAll =====");
         list = sellerDao.findAll();
 
         for (Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println("\n=== TEST 4: seller insert =====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+
+        sellerDao.insert(newSeller);
+        System.out.println("Insert! new id = " + newSeller.getId());
     }
 }
